86230b9843771478858e75650ac86e0460f7ac1a
NoxiousST <danielkristian268@gmail.com>
2024-07-15 11:17:23 +0700

    Act Seven (TP)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_ANON_KEY: string;
+    readonly VITE_MAPBOX_KEY: string;
+    readonly VITE_APP_SERVER: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
